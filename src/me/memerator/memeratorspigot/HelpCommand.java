b18d4e7c09efb3a567cc1e4076c7c1b746b3d9f7
Chew <chew@chew.pw>
2019-10-29 19:49:10 -0500

    add help command and plugin.yml

---
+++ src/me/memerator/memeratorspigot/HelpCommand.java
@@ -0,0 +1,21 @@
+package me.memerator.memeratorspigot;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelpCommand implements CommandExecutor {
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (sender instanceof Player) {
+
+      if(args.length == 0) {
+        sender.sendMessage("Welcome to Memerator Spigot.\nHave you linked your account? Be sure to with /memerator link [Memerator Account]");
+      }
+
+    } else {
+      sender.sendMessage("[Memerator] Only players may run this command.");
+    }
+
+    return true;
+  }
+}
--- src/me/memerator/memeratorspigot/MemeratorSpigot.java
+++ src/me/memerator/memeratorspigot/MemeratorSpigot.java
@@ -4,7 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MemeratorSpigot extends JavaPlugin {
   // Fired when plugin is first enabled
   public void onEnable() {
-    System.out.println("[Memerator] Memerator Plugin ready to go!");
+    if (getServer().getOnlineMode()) {
+      System.out.println("[Memerator] Memerator Plugin ready to go!");
+      this.getCommand("npns").setExecutor(new HelpCommand());
+    } else {
+      System.out.println("[Memerator] This plugin cannot work in Offline mode! Try again!");
+      getServer().getPluginManager().disablePlugin(this);
+    }
   }
   // Fired when plugin is disabled
   public void onDisable() {

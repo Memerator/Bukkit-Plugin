1e3550440f3493125518ee254dbdc29c1cfc6ef4
Chew <chew@chew.pw>
2019-10-29 19:39:29 -0500

    initial gitignore and base java file

---
+++ src/me/memerator/memeratorspigot/MemeratorSpigot.java
@@ -0,0 +1,13 @@
+package me.memerator.memeratorspigot;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MemeratorSpigot extends JavaPlugin {
+  // Fired when plugin is first enabled
+  public void onEnable() {
+    System.out.println("[Memerator] Memerator Plugin ready to go!");
+  }
+  // Fired when plugin is disabled
+  public void onDisable() {
+
+  }
+}

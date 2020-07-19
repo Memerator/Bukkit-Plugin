21dd050d5d4b10e6a7b51654b19ac4f8f748a9a1
Chew <chew@chew.pw>
2020-07-19 00:04:26 -0500

    Reindent with 4 spaces and ignore ide files

--- src/me/memerator/memeratorspigot/HelpCommand.java
+++ src/me/memerator/memeratorspigot/HelpCommand.java
@@ -5,17 +5,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class HelpCommand implements CommandExecutor {
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (sender instanceof Player) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
 
-      if(args.length == 0) {
-        sender.sendMessage("Welcome to §cMemerator§6 Spigot§r.\nHave you linked your account? Be sure to with /memerator link [Memerator Account]");
-      }
+            if(args.length == 0) {
+                sender.sendMessage("Welcome to §cMemerator§6 Spigot§r.\nHave you linked your account? Be sure to with /memerator link [Memerator Account]");
+            }
 
-    } else {
-      sender.sendMessage("[Memerator] Only players may run this command.");
+        } else {
+            sender.sendMessage("[Memerator] Only players may run this command.");
+        }
+
+        return true;
     }
-
-    return true;
-  }
 }
--- src/me/memerator/memeratorspigot/MemeratorSpigot.java
+++ src/me/memerator/memeratorspigot/MemeratorSpigot.java
@@ -2,18 +2,18 @@ package me.memerator.memeratorspigot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MemeratorSpigot extends JavaPlugin {
-  // Fired when plugin is first enabled
-  public void onEnable() {
-    if (getServer().getOnlineMode()) {
-      System.out.println("[Memerator] Memerator Plugin ready to go!");
-      this.getCommand("memerator").setExecutor(new HelpCommand());
-    } else {
-      System.out.println("[Memerator] This plugin cannot work in Offline mode! Try again!");
-      getServer().getPluginManager().disablePlugin(this);
+    // Fired when plugin is first enabled
+    public void onEnable() {
+        if (getServer().getOnlineMode()) {
+            System.out.println("[Memerator] Memerator Plugin ready to go!");
+            this.getCommand("memerator").setExecutor(new HelpCommand());
+        } else {
+            System.out.println("[Memerator] This plugin cannot work in Offline mode! Try again!");
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
-  }
-  // Fired when plugin is disabled
-  public void onDisable() {
+    // Fired when plugin is disabled
+    public void onDisable() {
 
-  }
+    }
 }
